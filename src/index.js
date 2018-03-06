0633defb2e2ae6526b5ec121ffdfe4db0df8cb18
Thomas Wiss <thomas.wiss@mittmedia.se>
2018-03-06 13:29:00 +0100

    Export default

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import EventTracker from 'eventTracker';
 import ConsoleTracker from 'tracker/consoleTracker';
 import HttpTracker from 'tracker/httpTracker';
 export { EventTracker, ConsoleTracker, HttpTracker };
+export default EventTracker;
